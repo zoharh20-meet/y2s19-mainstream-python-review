660125377fcc56840d0ee287a5949388ddf9addb
caboonie <37125297+caboonie@users.noreply.github.com>
2019-07-27 10:55:51 -0700

    Added tests for part 1

--- part2-test.py
+++ part2-test.py
@@ -2,4 +2,23 @@
 
 import part2
 
-## TODO: Write your test cases here. Refer to part1-test.py for an example.
+## Test 1. Tests the encode with 2 primes in range
+print("Test 1")
+if(part2.encode(643,5) == 643*5):
+	print("PASSED")
+else:
+	print("FAILED - should print 3215 for inputs of 643 and 5")
+  
+## Test 2. Tests the encode with 2 primes, where x is out of range
+print("Test 2")
+if(part2.encode(3,5) == None):
+	print("PASSED")
+else:
+	print("FAILED - should return None since x of 3 is not in range")
+  
+ ## Test 3. Tests the encode with 2 non-primes
+print("Test 2")
+if(part2.encode(501,6) == 3521):
+	print("PASSED")
+else:
+	print("FAILED - should return 3521. Since x of 501 is not prime, should increment to 503, and y of 6 should go to 7")
