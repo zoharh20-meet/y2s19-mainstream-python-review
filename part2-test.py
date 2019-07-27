7b442d0a81c598c4e96d0a9ee6a62237bf9d905d
caboonie <37125297+caboonie@users.noreply.github.com>
2019-07-27 10:59:54 -0700

    added test for decode

--- part2-test.py
+++ part2-test.py
@@ -17,8 +17,18 @@ else:
 	print("FAILED - should return None since x of 3 is not in range")
   
  ## Test 3. Tests the encode with 2 non-primes
-print("Test 2")
+print("Test 3")
 if(part2.encode(501,6) == 3521):
 	print("PASSED")
 else:
 	print("FAILED - should return 3521. Since x of 501 is not prime, should increment to 503, and y of 6 should go to 7")
+
+## Test 4. Tests the decode with product of 2 primes
+print("Test 3")
+if(part2.encode(66839) in [(751,89),(89,751)]):
+	print("PASSED")
+else:
+	print("FAILED - for the input of 66839, decode should return (751,89) in some order")
+, decode 
+
+##Write more test cases here
